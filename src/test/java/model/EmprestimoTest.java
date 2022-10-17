85c2f69fa291236be182b1cdbca41e2e45e336c4
luscas <lcky123hop@gmail.com>
2022-10-16 21:17:00 -0300

    LUC-20 Create EmprestimoServiceTest and ModelTest for system

--- src/test/java/model/EmprestimoTest.java
+++ src/test/java/model/EmprestimoTest.java
@@ -1,5 +1,20 @@
 package model;
 
-public class EmprestimoTest {
+import builder.UsuarioBuilder;
+import builder.EmprestimoBuilder;
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+class EmprestimoTest {
+    @Test
+    void naoEmprestaLivro() {
+        Usuario usuario = UsuarioBuilder
+                .umUsuario()
+                .construi();
+        Emprestimo emprestimo = EmprestimoBuilder
+                .umEmprestimo()
+                .emUsuario(usuario)
+                .construi();
+        assertEquals(null, emprestimo.getLivros()); //Teste estranho? Sim!KKKKKKKK
+    }
 }
--- src/test/java/model/UsuarioTest.java
+++ src/test/java/model/UsuarioTest.java
@@ -1,4 +1,17 @@
 package model;
 
-public class UsuarioTest {
+import builder.UsuarioBuilder;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UsuarioTest {
+    @Test
+    void estaEmDebito() {
+        Usuario usuario = UsuarioBuilder
+                .umUsuario()
+                .construi();
+        assertEquals(false,usuario.isEmDebito());
+    }
+
 }
